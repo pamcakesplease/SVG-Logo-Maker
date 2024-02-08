eab3623a5a2403218403803345fd0bd74888bfc0
pamcakesplease <pamelapimentel8@gmail.com>
2024-02-08 16:05:45 -0500

    fixed syntax error

--- index.js
+++ index.js
@@ -12,15 +12,21 @@ function writeToFile(fileName, answers) {
     if (answers.shape == "triangle")
     {
         shapeChoice = new triangle();
+        svgString += `<polygon points="250,60 100,400 400,400" fill="${answers.shapeBackgroundColor}"/>`;
     }
 
     else if (answers.shape == "circle")
     {
         shapeChoice = new circle();
+        svgString += `<circle r="45" cx="50" cy="50" fill="${answers.shapeBackoundColor}"/>`;
     }
 
     else  (answers.shape == "square")
     {
         shapeChoice = new square();
+        svgString += `<rect x="100" y="150" rx="0" ry="0" width="400" height="300" fill="${answers.shapeBackgroundColor}"/>`
     }
 }
+svgString += `<text x="100" y="20" text-anchor="middle" font-size="2em" fill="${answers.textColor}">${answers.text}</text>`;
+svgString += "</g>";
+svgString += "</svg>";
