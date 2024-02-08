c20a035cc5670197f3babd3316e09f7b55b1700c
pamcakesplease <pamelapimentel8@gmail.com>
2024-02-08 16:49:10 -0500

    completed index.js

--- index.js
+++ index.js
@@ -4,29 +4,70 @@ const { triangle, circle, square } = require("./lib/shapes");
 
 function writeToFile(fileName, answers) {
     let svgString = "";
-    svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
-    svgString += `${answers.shape}`;
+    svgString =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     svgString += "<g>";
+    svgString += `${answers.shape}`;
+  
 
-    let shapeChoice
-    if (answers.shape == "triangle")
-    {
-        shapeChoice = new triangle();
-        svgString += `<polygon points="250,60 100,400 400,400" fill="${answers.shapeBackgroundColor}"/>`;
+    let shapeChoice;
+    if (answers.shape === "triangle") {
+      shapeChoice = new triangle();
+      svgString += `<polygon points="250,60 100,400 400,400" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "circle") {
+      shapeChoice = new circle();
+      svgString += `<circle r="45" cx="50" cy="50" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+      shapeChoice = new square();
+      svgString += `<rect x="100" y="150" rx="0" ry="0" width="400" height="300" fill="${answers.shapeBackgroundColor}"/>`;
     }
+  
+    svgString += `<text x="200" y="160" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+  
 
-    else if (answers.shape == "circle")
-    {
-        shapeChoice = new circle();
-        svgString += `<circle r="45" cx="50" cy="50" fill="${answers.shapeBackoundColor}"/>`;
-    }
+    fs.writeFile(fileName, svgString, (err) => {
+      err ? console.log(err) : console.log("Generated logo.svg");
+    });
+  }
 
-    else  (answers.shape == "square")
-    {
-        shapeChoice = new square();
-        svgString += `<rect x="100" y="150" rx="0" ry="0" width="400" height="300" fill="${answers.shapeBackgroundColor}"/>`
-    }
+function promptUser() {
+  inquirer
+    .prompt([
+
+      {
+        type: "input",
+        message:
+          "What text would you like you logo to display?",
+        name: "text",
+      },
+      {
+        type: "input",
+        message:
+          "Choose text color by name or number",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["triangle", "circle", "square"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message:"Choose shapes background color by name or number",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
 }
-svgString += `<text x="100" y="20" text-anchor="middle" font-size="2em" fill="${answers.textColor}">${answers.text}</text>`;
-svgString += "</g>";
-svgString += "</svg>";
+
+promptUser();
